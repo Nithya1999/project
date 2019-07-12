ef34d2fbfeb9fce7bc9409da8794263d236d5b07
Nithya1999 <51734911+Nithya1999@users.noreply.github.com>
2019-07-12 18:42:14 +0530

    Create zero.py

---
+++ zero.py
@@ -0,0 +1,11 @@
+#Nithya
+n=int(input())
+a=list(map(int,input().split()))
+l=[]
+for i in range(0,len(a)):
+    for j in range(i+1,len(a)):
+        if(a[i]+a[j]==0):
+            l.append(a[i])
+            l.append(a[j])
+            break
+print(" ".join(map(str,l)))
