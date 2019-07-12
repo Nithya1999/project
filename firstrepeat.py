f33e19d8f967f57df56d6ff9f05e723d517ed342
Nithya1999 <51734911+Nithya1999@users.noreply.github.com>
2019-07-12 16:48:06 +0530

    Update firstrepeat.py

--- firstrepeat.py
+++ firstrepeat.py
@@ -1,15 +1,9 @@
 #Nithya
-n=int(input())
-a=input().split()
-l=[]
-for i in range(0,len(a)):
-    if a[i] not in l:
-        l.append(a[i])
-    else:
-        r=a[i]
+sandy1 = input()
+san1 = list(map(int,input().split()))
+tan1 = False
+for i in san1:
+    if san1.count(i) > 1:
+        tan1 = True
         break
-if r==0:
-    print('unique')
-else:
-    print(r)
-
+print(i if tan1 else "unique")
